d65e63c7f454d200ff7aa398691fe84e828c3f32
GavinRanasinghe29870 <gavinkisara@gmail.com>
2025-03-31 11:33:18 +0530

    Add project files.

---
+++ Group 41-Air Quality Monitoring Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group_41_Air_Quality_Monitoring_Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
